447520483cffe6305c14f8f600366ec0eda11d75
nani3013 <undrunarendra3013@gmai.com>
2024-08-26 18:20:57 +0530

    i created conversation between two persons

---
+++ 02-conservation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+#ctrl+s ....> save info purpose
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh. I am fine. How are you doing?"
+echo "Ramesh:: I am doing good Suresh. What's going on?"
+echo "Suresh:: I started learning Shell Script Ramesh
+
