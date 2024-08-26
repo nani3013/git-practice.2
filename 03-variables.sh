be641009b469e3a0c6c4f34dc8ec96df83f8eb98
nani3013 <undrunarendra3013@gmai.com>
2024-08-26 18:39:19 +0530

    i added new names

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #!/bin/bash
-PERSON1=Raheem
-PERSON2=Suresh
+PERSON1=bhanu 
+PERSON2=kishore
 
 echo "$PERSON1:: Hi ${PERSON2}, How are you?"
 echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
