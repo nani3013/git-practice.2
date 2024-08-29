9a234554c35d215076ee4a559d66816e0e4ae94f
nani3013 <undrunarendra3013@gmai.com>
2024-08-29 12:51:15 +0530

    shell

---
+++ 14. practice.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+USERID=$(id -u)
+R="/e[31m"
+G="/e[32m"
+Y="/e[33m"
+N='/e[0m"
+
+ CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then 
+        echo -e" please run the script with $R root privileges $N "
+    exit1
+    fi
+    }
+
+  VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e " $2 is ...$R failure $N "
+    exit1
+    else
+        echo -e " $2 is ....$G success $N "
+    fi
+      }
+
+ CHECK_ROOT
+
+            dnf list installed git
+
+    if [ $? -ne 0 ]
+    then 
+         echo " git is not installed it, going to install it."
+            dnf installed git -y
+    VALIDATE $? "installing git"
+    else 
+          echo " git is already install it .nothing do it."
+    fi 
